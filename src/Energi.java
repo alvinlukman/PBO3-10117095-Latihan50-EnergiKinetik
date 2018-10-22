c5432df10f48c4c9861c244a7b7de7bd4df01764
alvinlukmanNH <galinx20gmail.com>
2018-10-22 23:27:09 +0700

    energi

---
+++ src/Energi.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author 
+ * NAMA  :Alvin Lukman Nulhakim
+ * NIM   :10117095    
+ * KELAS :IF-3
+ */
+class Energi {
+   private int kecepatan;
+    private double massa;
+
+    public double getMassa() {
+        return massa;
+    }
+
+    public void setMassa(double massa) {
+        this.massa = massa;
+    }
+
+    public int getKecepatan() {
+        return kecepatan;
+    }
+
+    public void setKecepatan(int kecepatan) {
+        this.kecepatan = kecepatan;
+    }
+    public double hitungEnergiKinetik(){
+        return 0.5*massa*(kecepatan*kecepatan);
+        
+    }
+    /* karena dimulai dalam keadaan diem maka kecepatan nya menjadi nol*/
+    public double hitungUsaha(){
+        return 0.5*massa*(kecepatan*kecepatan) - 0.5*massa*(0*0);
+        
+    } 
+}
